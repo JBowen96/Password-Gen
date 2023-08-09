50691fd90846e70d6326bb79e5b05c8398a14ba9
Josh B <bowenpjoshua@gmail.com>
2023-08-09 16:33:29 -0400

    Added loop for each parameter

--- script.js
+++ script.js
@@ -15,24 +15,56 @@ window.addEventListener('load', function () {
 
     // vars for confirmed criteria
     var confirmLength = "";
-    var confirmSpecialChar;
-    var confirmNumber;
     var confirmAbcUpper;
     var confirmAbcLower;
+    var confirmNumber;
+    var confirmSpecialChar;
+
+
 
     // prompt for password length selection
     function genPass() {
         var confirmLength = prompt("How long would you like the password, 8-128?");
-        while(confirmLength < 8 || confirmLength > 128) {
+        //loop for length correction
+        while (confirmLength < 8 || confirmLength > 128) {
             alert("Your password must be 8-128 characters, Try again");
             var confirmLength = prompt("How long would you like the password, 8-128?");
         }
+
+        alert("You've selected " + confirmLength + " characters!")
+
+        //
+        var confirmAbcUpper = confirm("Select OK if you want to include uppercase characters");
+        var confirmAbcLower = confirm("Select OK if you want to include lowercase characters");
+        var confirmNumber = confirm("Select OK if you want to include numbers");
+        var confirmSpecialChar = confirm("Select OK fi you want to include special characters");
+
+        while (confirmAbcUpper === false && confirmAbcLower === false && confirmNumber === false && confirmSpecialChar === false) {
+            alert("Please select atleast one of the above password parameters");
+            var confirmAbcUpper = confirm("Select OK if you want to include uppercase characters");
+            var confirmAbcLower = confirm("Select OK if you want to include lowercase characters");
+            var confirmNumber = confirm("Select OK if you want to include numbers");
+            var confirmSpecialChar = confirm("Select OK fi you want to include special characters");
+        }
+
+        var passwordChar = []
+
+        if (confirmAbcUpper){
+            passwordChar = passwordChar.concat(abcUpper)
+        }
+        if (confirmAbcLower){
+            passwordChar = passwordChar.concat(abcLower)
+        }
+        if (confirmNumber){
+            passwordChar = passwordChar.concat(number)
+        }
+        if (confirmSpecialChar){
+            passwordChar = passwordChar.concat(specialChar)
+        }
+
+        
+
     }
-
-
-
-
-
 
 
 
