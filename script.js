15aae01b77d647845aead89e3f61f324ce0c2606
Josh B <bowenpjoshua@gmail.com>
2023-08-09 21:36:26 -0400

    Completed the rest of the javascript to parse through selections, create a password from a concat, then display this created password in the box on the html page

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function () {
 
 
     //prompt for clicking the button
-    document.querySelector('#button1').addEventListener('click', genPass);
+    document.querySelector('#button1').addEventListener('click', writePass);
 
     // vars for criteria
     var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -33,7 +33,7 @@ window.addEventListener('load', function () {
 
         alert("You've selected " + confirmLength + " characters!")
 
-        //
+        //section for user selected parameters
         var confirmAbcUpper = confirm("Select OK if you want to include uppercase characters");
         var confirmAbcLower = confirm("Select OK if you want to include lowercase characters");
         var confirmNumber = confirm("Select OK if you want to include numbers");
@@ -46,7 +46,7 @@ window.addEventListener('load', function () {
             var confirmNumber = confirm("Select OK if you want to include numbers");
             var confirmSpecialChar = confirm("Select OK fi you want to include special characters");
         }
-
+        //adds each parameter selected and randomizes by length submitted
         var passwordChar = []
 
         if (confirmAbcUpper){
@@ -61,12 +61,26 @@ window.addEventListener('load', function () {
         if (confirmSpecialChar){
             passwordChar = passwordChar.concat(specialChar)
         }
+            console.log(passwordChar)
+            
+            
+            var randoPass = ""
 
-        
+            for (var i = 0; i < confirmLength; i++) {
+                randoPass= randoPass + passwordChar[Math.floor(Math.random() * passwordChar.length)];
+                console.log(randoPass)
+            }
+            return randoPass;
 
     }
+    //write the created password to the password box on the html
+    function writePass() {
+        var password = genPass();
+        var passwordText = document.querySelector("#password");
 
-
+        passwordText.value = password;
+        
+    }
 
 }, false)
 
